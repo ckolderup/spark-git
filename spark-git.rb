37be6f655a58073efb616c553829b66c124addc0
Casey Kolderup <casey@kolderup.org>
2013-03-08 01:32:28 -0500

    first commit

---
+++ spark-git.rb
@@ -0,0 +1,38 @@
+#!/usr/bin/env ruby
+
+class Time
+  def week
+    strftime('%U').to_i
+  end
+end
+
+WEEK_SECONDS = 7 * 24 * 60 * 60
+
+def time_between?(t, min, max)
+  (t.year == min.year && t.week >= min.week) || (t.year == max.year && t.week <= max.week)
+end
+
+def git_hist(dir, ago_secs)
+  now = Time.now
+  min = now - ago_secs
+  `git --git-dir=#{dir}/.git --work-tree=#{dir} log --branches=* --author='#{ENV['USER']}' --since="#{ago_secs/WEEK_SECONDS} weeks ago" --pretty=format:'%at'`.split(" ").map {|line|
+    t = Time.at(line.split(' ').last.to_i)
+    time_between?(t, min, now) ? t : nil
+  }.compact.group_by {|x| x.strftime('%G%U') }.inject({}) {|res, (k, v)|
+    res[k] = v.size
+    res
+  }
+end
+
+def spark_vals(weeks, commits, min)
+  (0...weeks).map {|i|
+    commits[(min + WEEK_SECONDS * i).strftime('%G%U')] || 0
+  }.join(',')
+end
+
+def stats_since(dir, ago)
+  ago_secs = ago * 60 * 60 * 24 * 7
+  spark_vals(ago, git_hist(dir, ago_secs), Time.now - ago_secs)
+end
+
+print `spark #{stats_since(ARGV[0], ARGV[1].to_i)}`
